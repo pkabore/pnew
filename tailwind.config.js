c62ea663cb858a88823650ed6b0037dd7ebed20f
Paul Kaboré <pkabore@tutanota.com>
2022-05-24 13:46:52 +0000

    correct fonts path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,4 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-module.exports = {
-    content: [
-        'index.html',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: [...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    //plugins: [require('@tailwindcss/forms')],
-};
+//config.js
+module.exports = { content: [ 'index.html' ] };
